cfbcd80c15e4e7fb346c8eaad7ecae170444accb
alantheandroid <alessio.ilardi@gmail.com>
2022-06-21 19:19:37 +0200

    feat: add requested promise

--- exercise-1.js
+++ exercise-1.js
@@ -1,26 +1,34 @@
 const persons = [
   {
     id: 1,
-    firstName: 'Mario',
-    lastName: 'Rossi',
-    age: 25
+    firstName: "Mario",
+    lastName: "Rossi",
+    age: 25,
   },
   {
     id: 2,
-    firstName: 'Maria',
-    lastName: 'Verdi',
-    age: 32
+    firstName: "Maria",
+    lastName: "Verdi",
+    age: 32,
   },
   {
     id: 3,
-    firstName: 'Giovanni',
-    lastName: 'Rossi',
-    age: 35
-  }
+    firstName: "Giovanni",
+    lastName: "Rossi",
+    age: 35,
+  },
 ];
 
 function fetchPersonById(id) {
-  // code here
+  return new Promise((resolve, reject) => {
+    let fetchedPerson = persons.find((person) => person.id == id);
+    if (fetchedPerson) {
+      resolve(console.log(fetchedPerson));
+    }
+  });
 }
 
-// code here
+fetchPersonById(1);
+fetchPersonById(2);
+fetchPersonById(3);
+fetchPersonById(4); // this gets rejected and returns nothing
